f54afd580c3717f2472f8fe72ed8fa9290194b55
Leonid M <leonidms@gmail.com>
2011-02-24 21:58:44 +0100

    Initial comment

---
+++ src/test/java/me/doautomate/ExampleServletTest.java
@@ -0,0 +1,15 @@
+package me.doautomate;
+
+import org.junit.Test;
+
+/**
+ * Testing {@link me.doautomate.ExampleServlet}
+ */
+public class ExampleServletTest
+{
+    @Test
+    public void basic()
+    {
+        // add test here...
+    }
+}
